98892ca1e0bc6f1f04e4823742581922dd3b2535
Andrew Accuardi <andrewaccuardi@gmail.com>
2017-01-06 14:36:49 -0800

    Creates search function for ailments, and adds basic styling to doctor cards.

--- build/js/app.js
+++ build/js/app.js
@@ -1,3 +1,26 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.apiKey = "ce253951c9ed56a533788b9d229c92a9";	
+
+},{}],2:[function(require,module,exports){
+var apiKey = require('./../.env').apiKey;
+
+function Doctor() {
+}
+
+Doctor.prototype.getDoctors = function(medicalIssue, listDoctors) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(response) {
+     listDoctors(response.data)
+    })
+   .fail(function(error) {
+      console.log("fail");
+    });
+};
+
+
+exports.doctorModule = Doctor;
+
+},{"./../.env":1}],3:[function(require,module,exports){
 var Doctor = require("./../js/doctor.js").doctorModule;
 
 $(function() {
@@ -36,3 +59,5 @@ $(function() {
     doc.getDoctors(ailment, listDoctors);
   });
 });
+
+},{"./../js/doctor.js":2}]},{},[3]);
